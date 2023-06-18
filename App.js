c4eafee3dcf815aabc6d9bfdddd924a403b2afd7
kayyenl <yenyenlee9@gmail.com>
2023-06-18 19:45:42 +0800

    made the skeleton, and the bottomnav look

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import BottomNav from './components/BottomNav';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/TimberBG.png')} 
+      style={styles.container}
+    >
+      <View style={styles.overlay}>
+        <HomeScreen />
+        <BottomNav />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    resizeMode: 'cover',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0)', // Adjust the opacity (0.4 in this case) or color as desired
+    zIndex: 3,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
---
+++ components/BottomNav.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+export default function BottomNav() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.addTaskTab}>
+        <Image source={require('../assets/AddPost.png')} style={styles.iconTop} />
+      </View>
+      <View style={styles.bottomRow}>
+        <View style={styles.tab}>
+            <Image source={require('../assets/Skills.png')} style={styles.icon} />
+        </View>
+        <View style={styles.tab}>
+            <Image source={require('../assets/Bounty.png')} style={styles.icon} />
+        </View>
+        <View style={styles.tab}>
+            <Image source={require('../assets/Friends.png')} style={styles.icon} />
+        </View>
+        <View style={styles.tab}>
+          <Image source={require('../assets/Settings.png')} style={styles.icon} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: 'white',
+  },
+  addTaskTab: {
+    alignItems: 'center',
+    width: 170,
+    height: 170,
+    backgroundColor: 'white',
+    borderRadius: 100,
+    borderWidth: 5,
+    borderColor: '#F0CC49', // Adjust this color as needed
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 25, // Adjust this value as needed
+  },
+  bottomRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    borderTopWidth: 10,
+    borderColor: '#F2DE89', // Adjust this color as needed
+    backgroundColor: 'white',
+  },
+  tab: {
+    alignItems: 'center',
+    flex: 1,
+    borderRightWidth: 7,
+    paddingTop: 7,
+    borderRightColor: '#F2DE89',
+  },
+  iconPlaceholder: {
+    width: 71,
+    height: 60,
+    marginBottom: 4,
+    backgroundColor: 'lightgray',
+  },
+  icon: {
+    paddingTop: 5,
+    width: 76, // Adjust the width as needed
+    height: 76, // Adjust the height as needed
+    marginBottom: 5,
+  },
+  iconTop: {
+    paddingTop: 5,
+    width: 70, // Adjust the width as needed
+    height: 70, // Adjust the height as needed
+    marginBottom: 5,
+    bottom: 27,
+  },
+});
